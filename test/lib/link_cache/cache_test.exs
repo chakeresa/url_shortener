70393f5243fa0845754ec510af0305cdaf1e39ee
Alexandra Chakeres <alexchakeres@gmail.com>
2019-10-12 15:08:01 -0600

    Add cache test

---
+++ test/lib/link_cache/cache_test.exs
@@ -0,0 +1,14 @@
+defmodule LinkCacheTest do
+  use ExUnit.Case
+
+  test "caches and finds the correct data" do
+    assert LinkCache.Cache.fetch("A", fn ->
+             %{id: 1, long_url: "http://www.example.com"}
+           end) == %{id: 1, long_url: "http://www.example.com"}
+
+    assert LinkCache.Cache.fetch("A", fn -> "" end) == %{
+             id: 1,
+             long_url: "http://www.example.com"
+           }
+  end
+end
